f71d17aaaaf317fd39a72c82c3e10a76f7241306
ricardocolina <33906430+ricardocolina@users.noreply.github.com>
2023-01-27 13:57:08 +0100

    Actualizar pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,15 @@
-basic.forever(function () {
-	
+input.onButtonPressed(Button.A, function () {
+    envio = "" + randint(0, 4) + randint(0, 4)
+    radio.sendString(envio)
+    radio.setGroup(parseFloat(envio))
+    Escuchar = 1
 })
+radio.onReceivedString(function (receivedString) {
+    if (Escuchar == 1) {
+        basic.showString(receivedString)
+        Escuchar = 0
+    }
+})
+let Escuchar = 0
+let envio = ""
+radio.setGroup(1)
